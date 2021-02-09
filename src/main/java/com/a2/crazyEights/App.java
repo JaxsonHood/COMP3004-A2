bff53e75175f4e1c9a7214618bac9ac46f3c2dec
Jaxson Hood <jaxson.hood@hotmail.com>
2021-02-09 14:13:00 -0500

    Initial commit

---
+++ src/main/java/com/a2/crazyEights/App.java
@@ -0,0 +1,8 @@
+package com.a2.crazyEights;
+
+public class App {
+    public static void main( String[] args){
+        Game g = new Game();
+        g.hello();
+    }
+}
---
+++ src/main/java/com/a2/crazyEights/Game.java
@@ -0,0 +1,11 @@
+package com.a2.crazyEights;
+
+import java.io.Serializable;
+
+public class Game implements Serializable {
+    private static final long serialVersionUID = 123L;
+
+    public void hello(){
+        System.out.println("Hello YALL");
+    }
+}
